8d1d4f28fa0472933c932ab4fafd1afdd799f261
sumitsingh00 <sumit00134@gmail.com>
2026-05-06 22:58:42 +0530

    Push

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,8 +6,12 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",                    // local dev
+        "https://YOUR-APP.vercel.app"               // production
+    ],
     credentials: true
 }))
 
